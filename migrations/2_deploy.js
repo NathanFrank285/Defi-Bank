1dfbd5dfbed7c3057c396a88873e267d4624948c
NathanFrank285 <nathansfrank@yahoo.com>
2021-08-31 15:12:00 -0700

    added the deployer for dbank

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,15 +1,22 @@
 const Token = artifacts.require("Token");
 const dBank = artifacts.require("dBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
 	//deploy Token
 	await deployer.deploy(Token)
+	console.log(network)
+	console.log(accounts)
 
 	//assign token into variable to get it's address
-	let token = Token.address
+	let token = await Token.deployed()
+
 	//pass token address for dBank contract(for future minting)
+	await deployer.deploy(dBank, token.address)
 
 	//assign dBank contract into variable to get it's address
+	const dbank = await dBank.deployed()
 
 	//change token's owner/minter from deployer to dBank
+	await token.passMinterRole(dbank.address)
+
 };
